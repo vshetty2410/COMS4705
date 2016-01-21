acc398c2911f8db51cb09923adff6d31a32761e7
vshetty2410 <varun.shetty.1121@gmail.com>
2016-01-20 22:47:59 -0500

    Old Files removed

--- Homework2/envsetup.sh
+++
@@ -1,1 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/home/coms4705/.local/lib/python2.7/site-packages
--- Homework3/envsetup.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=/home/coms4705/.local/lib/python2.7/site-packages
-export NLTK_DATA=/home/coms4705/nltk_data
